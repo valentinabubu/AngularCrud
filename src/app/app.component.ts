a3ebdaaaf96d9293a37bc4d4c649ebdc946ffbd3
valentinabubu <escobargabrielapaz@mailcom>
2024-03-05 02:00:29 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularCrud';
+  employeeArray: Employee[] = [];
+  selectedEmployee: Employee = new Employee();
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchEmployees();
+  }
+
+  fetchEmployees(): void {
+    this.http.get<Employee[]>('http://localhost:3001/employees').subscribe(data => {
+      this.employeeArray = data;
+    });
+  }
+
+  openForEdit(employee: Employee): void {
+    this.selectedEmployee = { ...employee };
+  }
+
+  addOrEdit(): void {
+    if (this.selectedEmployee.id === 0) {
+      this.http.post('http://localhost:3001/employees', this.selectedEmployee).subscribe(() => {
+        this.fetchEmployees();
+        this.selectedEmployee = new Employee();
+      });
+    } else {
+      this.http.put(`http://localhost:3001/employees/${this.selectedEmployee.id}`, this.selectedEmployee).subscribe(() => {
+        this.fetchEmployees();
+        this.selectedEmployee = new Employee();
+      });
+    }
+  }
+
+  delete(): void {
+    if (confirm('¿Estás seguro de borrar?')) {
+      this.http.delete(`http://localhost:3001/employees/${this.selectedEmployee.id}`).subscribe(() => {
+        this.fetchEmployees();
+        this.selectedEmployee = new Employee();
+      });
+    }
+  }
 }
---
+++ src/app/models/employee.ts
@@ -0,0 +1,14 @@
+export class Employee {
+    id: number = 0;
+    name: string;
+    apellido: string;
+    fechaingreso: string;
+    
+
+    constructor() {
+        this.id = 0;
+        this.name = '';
+        this.apellido = '';
+        this.fechaingreso = '';
+    }
+}
